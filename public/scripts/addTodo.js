e6cb6742269543e2bc2b2f67ac109821dcf144de
Rashmi Gupta <rashmi224141@gmail.com>
2020-02-05 15:06:51 +0530

    Added feature of deleting the perticular item of a todo

--- public/scripts/addTodo.js
+++ public/scripts/addTodo.js
@@ -2,11 +2,29 @@ const itemsInHtml = function(task) {
   const {item, id, isDone} = task;
   const isChecked = isDone ? 'checked' : '';
   return `
-   <div class="showItems" >
-     <p class="tick">
-       <input type="checkbox" name="isDone" ${isChecked} id="${id}" />${item}
-     </p> 
+   <div class="showItems">
+       <input type="checkbox" name="isDone" class="editItem" ${isChecked} id="${id}" />
+       ${item}
+       <img src="../img/deleteIcon.png" class="delete" onclick="deleteItem()" id="${id}"/>
    </div><br/>`;
+};
+
+const addItems = function() {
+  const form = document.querySelector('.items');
+  const textarea = document.createElement('textarea');
+  textarea.cols = '50';
+  textarea.rows = '3';
+  textarea.required = true;
+  textarea.name = 'todoItem';
+  form.appendChild(textarea);
+  const br = document.createElement('br');
+  form.appendChild(br);
+};
+
+const removeItems = function() {
+  const form = document.querySelector('.items');
+  form.removeChild(form.lastChild);
+  form.removeChild(form.lastChild);
 };
 
 const getXmlHttpRequest = function(url, callback, args) {
@@ -24,7 +42,7 @@ const postXmlHttpRequest = function(url, body, callback, args) {
   let xhr = new XMLHttpRequest();
   xhr.onload = function() {
     if (this.status === 200) {
-      callback(JSON.parse(this.responseText), args);
+      callback(args);
     }
   };
   xhr.open('POST', url, true);
@@ -32,23 +50,17 @@ const postXmlHttpRequest = function(url, body, callback, args) {
   xhr.send(body);
 };
 
-const addItems = function() {
-  const form = document.querySelector('.items');
-  const textarea = document.createElement('textarea');
-  textarea.cols = '50';
-  textarea.rows = '3';
-  textarea.required = true;
-  textarea.name = 'todoItem';
-  form.appendChild(textarea);
-  const br = document.createElement('br');
-
-  form.appendChild(br);
+const deleteItem = function() {
+  const ItemId = event.target.getAttribute('id');
+  let todoId = document.querySelector('.todoDetail').getAttribute('id');
+  const body = `itemId=${ItemId}&todoId=${todoId}`;
+  postXmlHttpRequest('/deleteItem', body, testing, {todoId});
 };
-
-const removeItems = function() {
-  const form = document.querySelector('.items');
-  form.removeChild(form.lastChild);
-  form.removeChild(form.lastChild);
+const testing = function({todoId}) {
+  const detail = document.querySelector('.todoDetail');
+  detail.style['margin-top'] = '0vh';
+  detail.id = todoId;
+  getXmlHttpRequest('/todoList.json', showTodoItems, {todoId, detail});
 };
 
 const updateIsDoneStatus = function() {
@@ -57,7 +69,7 @@ const updateIsDoneStatus = function() {
   checkedBoxes = [...checkboxes].filter(checkbox => checkbox.checked);
   ids = checkedBoxes.map(box => box.getAttribute('id'));
   const body = `ids=${ids}&todoId=${todoId}`;
-  postXmlHttpRequest('/showList.html', body, removeDetail);
+  postXmlHttpRequest('/updateStatus', body, removeDetail);
 };
 
 const removeDetail = function() {
@@ -66,20 +78,20 @@ const removeDetail = function() {
 };
 
 const showDetail = function() {
-  const id = event.target.getAttribute('id');
+  const todoId = event.target.getAttribute('id');
   const detail = document.querySelector('.todoDetail');
   detail.style['margin-top'] = '0vh';
-  detail.id = id;
-  getXmlHttpRequest('/todoList.json', showTodoItems, {id, detail});
+  detail.id = todoId;
+  getXmlHttpRequest('/todoList.json', showTodoItems, {todoId, detail});
 };
 
 const showTodoItems = function(resText, args) {
-  resText = resText.filter(todo => todo.id == args.id).flat();
+  resText = resText.filter(todo => todo.id == args.todoId).flat();
   const todoItems = resText[0].todoItems.map(task => itemsInHtml(task));
   args.detail.innerHTML = `
     <div class="todo">
       <h1 class="titleHeading">${resText[0].title}</h1>${todoItems.join('')}
-      <button type="submit" onclick= updateIsDoneStatus()">save</button>
+      <button type="submit" onclick="updateIsDoneStatus()">save</button>
     </div>
     `;
 };
