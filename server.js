a2aba9260a4c1cc7dedd5698727fbd68facd5309
Rashmi Gupta <rashmi224141@gmail.com>
2020-02-12 10:29:51 +0530

    [noora/rashmi]injected sendResponse method into the response

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const defaultPort = 5000;
 
 const main = function(port = defaultPort) {
   const server = http.createServer();
-  server.on('request', app.serve.bind(app));
+  server.on('request', (req, res) => app.serve(req, res));
   server.listen(port, () =>
     process.stdout.write('server is listing', server.address())
   );
