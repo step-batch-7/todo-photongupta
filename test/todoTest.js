645b1b48996b48d73fed965555cd9cad43ce4245
Rashmi Gupta <rashmi224141@gmail.com>
2020-02-13 11:12:26 +0530

    [Rashmi/Noora] added test for todo class

---
+++ test/todoTest.js
@@ -0,0 +1,36 @@
+const assert = require('chai').assert;
+const {Todo} = require('../lib/todo');
+
+describe('Todo', function() {
+  describe('addItems', function() {
+    it('should add the given items into the todoItems', function() {
+      const todo = new Todo({title: 'homeWork', id: 1});
+      assert.strictEqual(todo.addItems(['maths', 'english']), 2);
+    });
+  });
+
+  describe('editTitle', function() {
+    it('should change the existing title', function() {
+      const todo = new Todo({title: 'homeWork', id: 1});
+      assert.strictEqual(todo.editTitle('classWork'), 'classWork');
+    });
+  });
+
+  describe('modifyItemList', function() {
+    it('should add the given items into the existing todo', function() {
+      const todo = new Todo({title: 'homeWork', id: 1});
+      assert.strictEqual(
+        todo.modifyItemList([{item: 'maths', isDone: false, id: 1}]),
+        1
+      );
+    });
+  });
+
+  describe('updateStatus', function() {
+    it('should change the status of items', function() {
+      const todo = new Todo({title: 'homeWork', id: 1});
+      todo.addItems(['maths']);
+      assert.strictEqual(todo.updateStatus([1, 2]), 1);
+    });
+  });
+});
