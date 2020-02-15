18e6ec449544ad81dc0988393195e5e791d4cd06
Rashmi Gupta <rashmi224141@gmail.com>
2020-02-15 16:27:54 +0530

    [noora/rashmi]used app.locals to store the data and depended functions

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 'use strict';
 
+const {writeFileSync} = require('fs');
+const TodoLists = require('./lib/todoLists');
+const USER_STORE = './dataBase/userCredentials.json';
+const TODO_STORE = './dataBase/todoList.json';
+const todoLists = require(TODO_STORE);
+const userCredentials = require(USER_STORE);
 const {app} = require('./lib/routes');
 
 const defaultPort = 5000;
 
 const main = function(port = defaultPort) {
+  app.locals = {
+    todoLists: TodoLists.load(todoLists),
+    writer: writeFileSync,
+    userCredentials,
+    USER_STORE,
+    TODO_STORE
+  };
   app.listen(port);
 };
 
